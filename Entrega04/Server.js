ee76fb79fdde50f044aa3434efa8fdac447704e8
benjamin zamora <benjazamo@gmail.com>
2024-04-07 01:15:02 -0400

    agregando carts

--- Entrega04/Server.js
+++ Entrega04/Server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productsRouter from './src/routes/products.route.js'
+import cartsRouter from './src/routes/carts.route.js'
 import { __dirname } from './src/Utils/Statics.js';
 
 const app = express()
@@ -9,9 +10,11 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static(__dirname+'/public'))
 
 app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
+
 
 app.get('/', (req, res) => {
-    res.send(`Intenta con esta ruta "/products" ;D`)
+    res.send(`Intenta con esta ruta "api/products" ;D`)
 })
 
 app.listen(8080, error => {
